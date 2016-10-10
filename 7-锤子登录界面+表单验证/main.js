b8220933f9311bba3b3975c716c6bb2c06b13465
zoubingwu <zoubingwu@gmail.com>
2016-10-10 20:43:27 +0800

    files reorgnized

---
+++ 7-锤子登录界面+表单验证/main.js
@@ -0,0 +1,93 @@
+$(function(){
+	// add a blue border when input is focused
+	$(".input input").focus(function(){
+		$(this).parent().css("border","1px solid #3266F2");
+	})
+	$(".input input").blur(function(){
+		$(this).parent().css("border","1px solid #CCC");
+	});
+
+	// change input opacity when is hovered
+	$(".input").hover(function(){
+		$(this).css("opacity","1");
+	},function(){
+		$(this).css("opacity","0.618");
+	});
+
+	// change background image when checkbox is toggled
+	$(".auto").click(function(){
+		$(".checkbox").toggleClass("disabled active");
+	});
+
+	// form validation
+	var f1 = false;
+	var f2 = false;
+
+	$(".input input:text").keyup(function(){
+		var str = this.value;
+		if (str != "" ) {
+			var regMail = /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/;
+			var regPhone = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/;
+			if(!regMail.test(str) && !regPhone.test(str)){
+				f1 = false;
+				return false;
+			} else {
+				f1 = true;
+				$(this).siblings(".warning").hide();
+			}
+		} else {
+			f1 = false;
+			return false;
+		}
+	});
+
+	$(".input input:password").keyup(function(){
+		var str = this.value;
+		if (str != "") {
+			var regPw = /^\w+$/;
+			if(!regPw.test(str)){
+				f2 = false;
+				return false;
+			} else {
+				f2 = true;
+				$(this).siblings(".warning").hide();
+			}
+		} else {
+			f2 = false;
+			return false;
+		}
+	})
+
+	$(".input input:text").blur(function(){
+		if(this.value != "") {
+			if(!f1){
+				$(".warning:eq(0)").show();
+			} else {
+				$(".warning:eq(0)").hide();
+			}
+		} else {
+			$(".warning:eq(2)").show();
+		}
+	})
+
+	$(".input input:password").blur(function(){
+		if(this.value != "") {
+			if(!f2){
+				$(".warning:eq(4)").show();
+			} else {
+				$(".warning:eq(4)").hide();
+			}
+		} else {
+			$(".warning:eq(3)").show();
+		}
+	})
+
+
+	$(".input input:text, .input input:password").keyup(function(){
+		if ( f1 && f2 ) {
+			$(".btn button").removeClass("disabled").addClass("active");
+		} else {
+			$(".btn button").removeClass("active").addClass("disabled");
+		}			
+	});
+})
