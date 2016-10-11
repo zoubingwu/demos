53f30b084b1bac5fb7d188f15b87a2a8efbb92a8
zoubingwu <zoubingwu@gmail.com>
2016-10-11 22:40:51 +0800

    added a draggable window demo

--- 10-draggable window/main.js
+++ 10-draggable window/main.js
@@ -1,5 +1,19 @@
 window.onload = function(){
-	var navbar = document.getElementsByTagName("span")[0];
+	var bar = document.getElementsByTagName("span")[0];
 	var box = document.getElementsByTagName("div")[0];
+
+	bar.onmousedown = function(event){
+		var x = event.clientX - box.offsetLeft;
+		var y = event.clientY - box.offsetTop;
+
+		bar.onmousemove = function(e){
+			box.style.left =  (e.clientX - x) + "px";
+			box.style.top = (e.clientY - y) + "px";
+		}
+		bar.onmouseup = function(e){
+			bar.onmousemove = null;
+			bar.onmouseup = null;
+		}
+	}
 
 }
