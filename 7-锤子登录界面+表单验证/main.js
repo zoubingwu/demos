a7a868a55b2e7dc7ae4bea311e18bd939705c67d
zoubingwu <zoubingwu@gmail.com>
2016-10-13 12:44:48 +0800

    bugs fixed
    1.hide other warning infos after validation .
    2.added form submit method

--- 7-锤子登录界面+表单验证/main.js
+++ 7-锤子登录界面+表单验证/main.js
@@ -23,6 +23,7 @@ $(function(){
 	var f1 = false;
 	var f2 = false;
 
+	// username
 	$(".input input:text").keyup(function(){
 		var str = this.value;
 		if (str != "" ) {
@@ -30,59 +31,59 @@ $(function(){
 			var regPhone = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/;
 			if(!regMail.test(str) && !regPhone.test(str)){
 				f1 = false;
-				return false;
 			} else {
 				f1 = true;
 				$(this).siblings(".warning").hide();
 			}
 		} else {
 			f1 = false;
-			return false;
 		}
 	});
 
+	//password
 	$(".input input:password").keyup(function(){
 		var str = this.value;
 		if (str != "") {
 			var regPw = /^\w+$/;
 			if(!regPw.test(str)){
 				f2 = false;
-				return false;
 			} else {
 				f2 = true;
 				$(this).siblings(".warning").hide();
 			}
 		} else {
 			f2 = false;
-			return false;
 		}
 	})
 
+
+	//username valiadation
 	$(".input input:text").blur(function(){
 		if(this.value != "") {
 			if(!f1){
-				$(".warning:eq(0)").show();
+				$(".warning:eq(0)").show().siblings(".warning").hide();
 			} else {
 				$(".warning:eq(0)").hide();
 			}
 		} else {
-			$(".warning:eq(2)").show();
+			$(".warning:eq(2)").show().siblings(".warning").hide();
 		}
 	})
 
+	// password validation
 	$(".input input:password").blur(function(){
 		if(this.value != "") {
 			if(!f2){
-				$(".warning:eq(4)").show();
+				$(".warning:eq(4)").show().siblings(".warning").hide();
 			} else {
 				$(".warning:eq(4)").hide();
 			}
 		} else {
-			$(".warning:eq(3)").show();
+			$(".warning:eq(3)").show().siblings(".warning").hide();
 		}
 	})
 
-
+	// disable or activate the button
 	$(".input input:text, .input input:password").keyup(function(){
 		if ( f1 && f2 ) {
 			$(".btn button").removeClass("disabled").addClass("active");
@@ -90,4 +91,13 @@ $(function(){
 			$(".btn button").removeClass("active").addClass("disabled");
 		}			
 	});
+
+	//submit form
+	$(".btn button").click(function(){
+		if(f1 && f2) {
+			$("form").submit();
+		} else {
+			return false;
+		}
+	})
 })
